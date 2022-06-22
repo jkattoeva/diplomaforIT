fe5d581910740969271ec9200e1c50559f0da21e
Janetta Kataoeva <jkattoeva@gmail.com>
2022-06-23 02:18:56 +0600

    Added SignUp pages in proect

---
+++ src/pages/SignUp.js
@@ -0,0 +1,15 @@
+import "./pages.css";
+import Header from "../components/Header/Header";
+import headerImage from "../assets/headersgnup.jpg";
+
+function SignUp() {
+  return (
+    <>
+    <Header title="You can Sign up in our store"
+     image={headerImage}>
+    Additionally, fashion has often dictated many design elements, such as whether shoes have very high heels or flat ones. Contemporary footwear varies widely in style, complexity and cost. 
+    </Header>
+    </>
+  );
+}
+ export default SignUp;
